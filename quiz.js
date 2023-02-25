c60145104708e3b4e6bb25be11e12dc90e1f844f
Daylightgamingstudios <96274605+Daylightgamingstudios@users.noreply.github.com>
2023-02-24 17:58:33 -0700

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,1 +1,27 @@
+// Define the quiz questions and answers
+const quizData = [
+  {
+    question: "What is the capital of Afghanistan?",
+    answer: "a"
+  },
+  {
+    question: "When did Afghanistan become independent?",
+    answer: "a"
+  }
+];
 
+// Function to check the user's answers
+function checkAnswers() {
+  const form = document.getElementById("quiz-form");
+  let score = 0;
+
+  for (let i = 0; i < quizData.length; i++) {
+    const answer = form.elements["q" + (i + 1)].value;
+    if (answer === quizData[i].answer) {
+      score++;
+    }
+  }
+
+  const feedback = document.getElementById("quiz-feedback");
+  feedback.innerHTML = "You scored " + score + " out of " + quizData.length;
+}
